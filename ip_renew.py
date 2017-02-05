d89672d3c93215129db8d617c685caa6113c8213
jaketarnow <jtarnow@dons.usfca.edu>
2017-02-04 18:36:22 -0800

    editing to work with Stem

--- ip_renew.py
+++ ip_renew.py
@@ -1,24 +1,9 @@
-# Implemented from http://sacharya.com/crawling-anonymously-with-tor-in-python/
-from TorCtl import TorCtl
-import urllib2
+# TorCtl is depreciated...use Stem
+from stem import Signal
+from stem.control import Controller
 
-user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X x.y; rv:10.0) Gecko/20100101 Firefox/10.0'
-headers = {'User-Agent':user_agent}
-
-def request(url):
-	def _set_urlproxy():
-		proxy_support = urllib2.ProxyHandler({"http" : "127.0.0.1:8118"})
-		opener = urllib2.build_opener(proxy_support)
-	_set_urlproxy()
-	request = urllib2.Request(url, None, headers)
-	return urllib2.urlopen(request).read()
-
-def renew_connection():
-	conn = TorCtl.connect(controlAddr = "127.0.0.1", controlPort = 9050, passphrase = "")
-	conn.send_signal("NEWNYM")
-	conn.close()
-
-for i in range(0, 10):
-	renew_connection()
-	print request("https://blockchain.info/")
-	
+def set_new_ip():
+	# Change IP Using Tor
+	with Controller.from_port(port = 9051) as controller:
+		controller.authenticate()
+		controller.signal(Signal.NEWNYM)
