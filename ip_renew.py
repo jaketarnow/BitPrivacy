db2102baa8df14a58c6e3b48f7013562b462bbe8
jaketarnow <jtarnow@dons.usfca.edu>
2017-02-05 22:12:20 -0800

    adding req layer

--- ip_renew.py
+++ ip_renew.py
@@ -1,5 +1,6 @@
 # TorCtl is depreciated...use Stem
 from stem import Signal
+import requests
 from stem.control import Controller
 
 def set_new_ip():
@@ -7,3 +8,9 @@ def set_new_ip():
 	with Controller.from_port(port = 9051) as controller:
 		controller.authenticate()
 		controller.signal(Signal.NEWNYM)
+
+def gather_reqs():
+	local_proxy = '127.0.0.1:8118'
+	http_proxy = {'http' : local_proxy, 'https' : local_proxy}
+
+	current_ip = requests.get(url = 'http://icanhazip.com/', proxies = http_proxy, verify = False)
