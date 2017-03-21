fd1e525e01656eee42374d7307ec855d7457364c
jaketarnow <jtarnow@dons.usfca.edu>
2017-03-21 09:42:22 -0700

    debugging browser visits

--- connecting_tor.py
+++ connecting_tor.py
@@ -1,4 +1,6 @@
 import stem.process
+import stem.connection
+import stem.socket
 from stem import Signal
 from stem.control import Controller
 from splinter import Browser
@@ -6,24 +8,18 @@ from splinter import Browser
 proxyIP = "127.0.0.1"
 proxyPort = 9050
 #http://www.thedurkweb.com/automated-anonymous-interactions-with-websites-using-python-and-tor/
-proxy_settings = {"network.proxy.type":1,
-	"network.proxy.ssl": proxyIP,
-	"network.proxy.ssl_port": proxyPort,
-	"network.proxy.socks": proxyIP,
-	"network.proxy.socks_port": proxyPort,
-	"network.proxy.socks_remote_dns": True,
-	"network.proxy.ftp": proxyIP,
-	"network.proxy.ftp_port": proxyPort
-}
-browser = Browser('firefox', profile_preferences=proxy_settings)
+browser = Browser('firefox')
+
+def switchIP():
+	with Controller.from_port(port=proxyPort) as controller:
+		controller.authenticate()
+		bytes_read = controller.get_info("traffic/read")
+		bytes_written = controller.get_info("traffic/written")
+		print("My Tor relay has read %s bytes and written %s." % (bytes_read, bytes_written))
+		#controller.signal(Signal.NEWNYM)
+		# NEWNYM used to send to Tor connection
 
 for x in range(50):
 	browser.visit("https://check.torproject.org/")
 	switchIP()
 	time.sleep(5)
-
-def switchIP():
-	with Controller.from_port(port=proxyPort) as controller:
-		controller.authenticate()
-		controller.signal(Signal.NEWNYM)
-		# NEWNYM used to send to Tor connection
